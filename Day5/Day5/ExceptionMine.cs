1e335070c9935e58d67396b6e6bcfe1fb9cc7908
cnithin19 <cnithin19@gmail.com>
2025-07-27 22:58:06 +0530

    Day5,6

---
+++ Day5/Day5/ExceptionMine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class ExceptionMine
+    {
+
+  
+        static void Main()
+        {
+            try
+            {
+                int x = 7, y = 0;
+                Console.WriteLine(x/y);
+            }
+            catch(MyException e) {
+                MyException(e);
+            }
+        }
+
+
+    }
+}
---
+++ EmployProject.Models/EmployProject.Models/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployProject.Models
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
